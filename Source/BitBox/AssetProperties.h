abeeff30b5f95087a963cd08407663eb7b033e9d
Chris Roberts <cprandroid@gmail.com>
2025-01-20 08:14:23 -0800

    Added missing properties to AssetProperties

--- Source/BitBox/AssetProperties.h
+++ Source/BitBox/AssetProperties.h
@@ -22,6 +22,15 @@ public:
     void setKeyRangeTop (int keyRangeTop, bool includeSelfCallback) { setValue (keyRangeTop, KeyRangeTopPropertyId, includeSelfCallback); }
     int getKeyRangeTop () { return getValue<int> (KeyRangeTopPropertyId); }
 
+    void setVelRoot (int velRoot, bool includeSelfCallback) { setValue (velRoot, VelRootPropertyId, includeSelfCallback); }
+    int getVelRoot () { return getValue<int> (VelRootPropertyId); }
+
+    void setVelRangeBottom (int velRangeBottom, bool includeSelfCallback) { setValue (velRangeBottom, VelRangeBottomPropertyId, includeSelfCallback); }
+    int getVelRangeBottom () { return getValue<int> (VelRangeBottomPropertyId); }
+
+    void setVelRangeTop (int velRangeTop, bool includeSelfCallback) { setValue (velRangeTop, VelRangeTopPropertyId, includeSelfCallback); }
+    int getVelRangeTop () { return getValue<int> (VelRangeTopPropertyId); }
+
     void setAssSrcRow (int assSrcRow, bool includeSelfCallback) { setValue (assSrcRow, AssSrcRowPropertyId, includeSelfCallback); }
     int getAssSrcRow () { return getValue<int> (AssSrcRowPropertyId); }
 
@@ -33,6 +42,9 @@ public:
     static inline const juce::Identifier RootNotePropertyId { "rootNote" };
     static inline const juce::Identifier KeyRangeBottomPropertyId { "keyRangeBottom" };
     static inline const juce::Identifier KeyRangeTopPropertyId { "keyRangeTop" };
+    static inline const juce::Identifier VelRootPropertyId { "velRoot" };
+    static inline const juce::Identifier VelRangeBottomPropertyId { "velRangeBottom" };
+    static inline const juce::Identifier VelRangeTopPropertyId { "velRangeTop" };
     static inline const juce::Identifier AssSrcRowPropertyId { "assSrcRow" };
     static inline const juce::Identifier AssSrcColPropertyId { "assSrcCol" };
 
@@ -40,6 +52,9 @@ public:
     std::function<void (int rootNote)> onRootNoteChange;
     std::function<void (int keyRangeBottom)> onKeyRangeBottomChange;
     std::function<void (int keyRangeTop)> onKeyRangeTopChange;
+    std::function<void (int velRoot)> onVelRootChange;
+    std::function<void (int velRangeBottom)> onVelRangeBottomChange;
+    std::function<void (int velRangeTop)> onVelRangeTopChange;
     std::function<void (int assSrcRow)> onAssSrcRowChange;
     std::function<void (int assSrcCol)> onAssSrcColChange;
 
@@ -59,6 +74,12 @@ private:
                 onKeyRangeBottomChange (getKeyRangeBottom ());
             else if (property == KeyRangeTopPropertyId && onKeyRangeTopChange != nullptr)
                 onKeyRangeTopChange (getKeyRangeTop ());
+            else if (property == VelRootPropertyId && onVelRootChange != nullptr)
+                onVelRootChange (getVelRoot ());
+            else if (property == VelRangeBottomPropertyId && onVelRangeBottomChange != nullptr)
+                onVelRangeBottomChange (getVelRangeBottom ());
+            else if (property == VelRangeTopPropertyId && onVelRangeTopChange != nullptr)
+                onVelRangeTopChange (getVelRangeTop ());
             else if (property == AssSrcRowPropertyId && onAssSrcRowChange != nullptr)
                 onAssSrcRowChange (getAssSrcRow ());
             else if (property == AssSrcColPropertyId && onAssSrcColChange != nullptr)
